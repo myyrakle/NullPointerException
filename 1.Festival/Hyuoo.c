85fee094ee5fae71ecb2abe4c8bc1e2ceac7fdf2
Hyuoo <43870227+Hyuoo@users.noreply.github.com>
2019-01-07 02:29:49 +0900

    4684ms

--- 1.Festival/Hyuoo.c
+++ 1.Festival/Hyuoo.c
@@ -3,18 +3,15 @@
 int main() {
 	int C, N, L, cost[1000] = { 0 };
 	int n, i,j;
-	float min, sum;
+	double min, sum;
 	//C<=100, 1<=L<=N<=1000, cost<=100
-  
-	scanf_s("%d", &C);
-  
+	scanf("%d", &C);
 	while (C--) {
-		scanf_s("%d %d",&N, &L);
+		scanf("%d %d",&N, &L);
 		for (n = 0; n < N; n++)
-			scanf_s("%d", &cost[n]);
+			scanf("%d", &cost[n]);
 		//입력끝
-	
-  		min = 100000;
+		min = 100000;
 		for (i = L; i <= N; i++) {
 			for (j = 0; j <= (N - i); j++) {
 				for (sum=0,n = 0; n < i; n++)
@@ -25,6 +22,4 @@ int main() {
 		}
 		printf("%.10f\n", min);
 	}
-  
 }
-//4684ms
