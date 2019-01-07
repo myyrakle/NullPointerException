61c49aa0541b51511fdda3ac96da8f7b21975575
gurumee328 <38632249+gurumee328@users.noreply.github.com>
2019-01-07 16:03:23 +0900

    624ms

--- 1.Festival/gurumee328.cpp
+++ 1.Festival/gurumee328.cpp
@@ -16,44 +16,38 @@
 using namespace std;
 
 int main(void) {
-  std::ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  int T;
-  cin >> T;
-  for (int tc = 1; tc <= T; tc++) {
-    int N, L;
-    cin >> N >> L;
+    int T;
+    cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        int N, L;
+        cin >> N >> L;
 
-    vector<double> costs;
-    int buf;
-    for (int i = 0; i < N; i++) {
-      cin >> buf;
-      costs.push_back(buf);
+        vector<vector<double>> arr(N + 1, vector<double>(N + 1, 1e5));
+
+        for (int i = 0; i < N; i++) {
+            cin >> arr[1][i + 1];
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = i; j <= N; j++) {
+                arr[i][j] = arr[1][j - i + 1] + arr[i - 1][j];
+            }
+        }
+
+        double ret = 1e10;
+        for (int i = L; i <= N; i++) {
+            sort(arr[i].begin(), arr[i].begin() + N + 1);
+            ret = min(ret, arr[i][0] / i);
+        }
+
+        cout << fixed;
+        cout.precision(8);
+        cout << ret << endl;
     }
 
-    vector<vector<double>> arr(costs.size() + 1,
-                               vector<double>(costs.size() + 1, 1e5));
-    for (int i = 0; i < costs.size(); i++) {
-      arr[1][i + 1] = costs[i];
-    }
-
-    for (int i = 2; i <= costs.size(); i++) {
-      for (int j = i; j <= costs.size(); j++) {
-        arr[i][j] = arr[1][j - i + 1] + arr[i - 1][j];
-      }
-    }
-
-    double ret = 1e10;
-    for (int i = L; i <= costs.size(); i++) {
-      sort(arr[i].begin(), arr[i].end());
-      ret = min(ret, arr[i][0] / i);
-    }
-    cout << fixed;
-    cout.precision(8);
-    cout << ret << endl;
-  }
-
-  return 0;
+    return 0;
 }
